d31fbb0f7dd8fe7d61b409e1b7db374cdde007a0
Eric-Vu-52 <eric.vu@playstudios.asia>
2025-06-08 12:25:01 +0700

    #add login

---
+++ firebase-config.js
@@ -0,0 +1,12 @@
+const firebaseConfig = {
+    apiKey: "AIzaSyBCq4YtMe2_TYDyGw7i0sE4m_XQxoGbxhE",
+    authDomain: "test-login-20806.firebaseapp.com",
+    projectId: "test-login-20806",
+    storageBucket: "test-login-20806.firebasestorage.app",
+    messagingSenderId: "695706278210",
+    appId: "1:695706278210:web:59e032063f4edee85fd0db",
+    measurementId: "G-G36GS4B64E"
+    };
+
+// Initialize Firebase
+firebase.initializeApp(firebaseConfig);
